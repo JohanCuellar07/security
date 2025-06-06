57a9fa76dd1e5835e16de3204405a9cf0db8c4d3
Johan Cuellar <cuellarjohan.07@gmail.com>
2025-06-06 18:50:38 -0500

    Add Role Service and Controller

--- src/main/java/com/sena/security/controller/RoleController.java
+++ src/main/java/com/sena/security/controller/RoleController.java
@@ -1,10 +1,54 @@
 package com.sena.security.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sena.security.DTO.RoleDTO;
+import com.sena.security.DTO.responseDTO;
+import com.sena.security.service.RoleService;
 
 @RestController
 @RequestMapping("/role")
 public class RoleController {
+    @Autowired
+    private RoleService roleService;
 
+    @PostMapping("/")
+    public ResponseEntity<Object> registerRole(@RequestBody RoleDTO roleDTO) {
+        responseDTO respuesta = roleService.save(roleDTO);
+        return new ResponseEntity<>(respuesta, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateRole(@PathVariable int id, @RequestBody RoleDTO roleDTO) {
+        responseDTO respuesta = roleService.updateRole(id, roleDTO);
+        return new ResponseEntity<>(respuesta, HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Object> getAllRoles() {
+        var listRoles = roleService.findAll();
+        return new ResponseEntity<>(listRoles, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneRole(@PathVariable int id) {
+        var role = roleService.findById(id);
+        return new ResponseEntity<>(role, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteRole(@PathVariable int id) {
+        responseDTO respuesta = roleService.deleteRole(id);
+        return new ResponseEntity<>(respuesta, HttpStatus.OK);
+    }
 }
--- src/main/java/com/sena/security/service/RoleService.java
+++ src/main/java/com/sena/security/service/RoleService.java
@@ -1,8 +1,104 @@
 package com.sena.security.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import com.sena.security.DTO.RoleDTO;
+import com.sena.security.DTO.responseDTO;
+import com.sena.security.model.Role;
+import com.sena.security.repository.IRole;
 
 @Service
 public class RoleService {
+    @Autowired
+    private IRole data;
 
+    public List<Role> findAll() {
+        return data.findAll();
+    }
+
+    public Optional<Role> findById(int id) {
+        return data.findById(id);
+    }
+
+    public responseDTO deleteRole(int id) {
+        if (!findById(id).isPresent()) {
+            responseDTO respuesta = new responseDTO(
+                HttpStatus.NOT_FOUND.toString(), 
+                "Role not found"
+            );
+            return respuesta;
+        };
+        data.deleteById(id);
+        responseDTO respuesta = new responseDTO(
+            HttpStatus.OK.toString(), 
+            "Role deleted"
+        );
+        return respuesta;
+    }
+
+    public responseDTO save(RoleDTO roleDTO) {
+        if (roleDTO.getName().length() < 1 || roleDTO.getName().length() > 50) {
+            responseDTO respuesta = new responseDTO(
+                HttpStatus.BAD_REQUEST.toString(), 
+                "Role name can't be empty or more than 50 characters"
+            );
+            return respuesta;
+        }
+        if (roleDTO.getDescription().length() > 255) {
+            responseDTO respuesta = new responseDTO(
+                HttpStatus.BAD_REQUEST.toString(), 
+                "Role description can't be more than 255 characters"
+            );
+            return respuesta;
+        }
+        Role roleRegister = convertToModel(roleDTO);
+        data.save(roleRegister);
+        responseDTO respuesta = new responseDTO(
+            HttpStatus.OK.toString(), 
+            "Role created"
+        );
+        return respuesta;
+    }
+
+    public responseDTO updateRole(int id, RoleDTO roleDTO) {
+        Optional<Role> roleOpt = data.findById(id);
+        if (!roleOpt.isPresent()) {
+            responseDTO respuesta = new responseDTO(
+                HttpStatus.NOT_FOUND.toString(), 
+                "Role not found"
+            );
+            return respuesta;
+        }
+        Role existingRole = roleOpt.get();
+        existingRole.setName(roleDTO.getName());
+        existingRole.setDescription(roleDTO.getDescription());
+        data.save(existingRole);
+        responseDTO respuesta = new responseDTO(
+            HttpStatus.OK.toString(), 
+            "Role updated"
+        );
+        return respuesta;
+    }
+
+    public RoleDTO convertToDTO(Role role) {
+        RoleDTO roleDTO = new RoleDTO(
+            role.getName(),
+            role.getDescription()
+        );
+        return roleDTO;
+    }
+
+    public Role convertToModel(RoleDTO roleDTO) {
+        Role role = new Role(
+            0,
+            roleDTO.getName(),
+            roleDTO.getDescription()
+        );
+        return role;
+    }
 }
